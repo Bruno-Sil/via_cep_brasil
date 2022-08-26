4c96f19105b1dc076b08b909fa0d6bc606e05b6b
Carlos Batista <carloscoffeepy@gmail.com>
2022-08-25 21:25:34 -0300

    change text and more test

--- test/via_cep_brasil_test.dart
+++ test/via_cep_brasil_test.dart
@@ -1,11 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:via_cep_brasil/via_cep_brasil.dart';
 
-
 void main() {
-  test('consumir api viacep', () async{
-    final viacepSearch = await ViaCepSearch.getInstance("69005180");
-    expect(viacepSearch, isA<ViaCepSearch>()); 
+  test('Deve retornar um ViaCepSearch', () async {
+    final viacepSearch = await ViaCepSearch.searchCep("69005180");
+    expect(viacepSearch, isA<ViaCepSearch>());
+  });
+
+  
+  test('Deve retornar um Exception caso o cep seja inválido', () async {
+    try {
+      await ViaCepSearch.searchCep("abcd");
+    } on Exception catch (error) {
+      expect(error, isA<Exception>());
+      expect(error.toString(), matches("O CEP é inválido"));
+    }
   });
 }
